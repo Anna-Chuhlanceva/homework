0468d7921d2fa1de95c5b78b6f9ccd12a177edc4
Anna-Chuhlanceva <an.chuh89@gmail.com>
2024-11-17 21:40:55 +0500

    Домашка с возрастом

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+name = 'Anna'
+print ('Name:', name)
+age = 35
+print('Age:' , age)
+age = age+1
+print ('New Age:' , age)
+is_student = 5 < 10
+print ('Is Student:' , is_student)
