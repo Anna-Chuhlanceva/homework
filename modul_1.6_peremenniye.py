4a66b95647d1e01673b6d89492399e4b283260de
Anna-Chuhlanceva <an.chuh89@gmail.com>
2024-11-19 20:04:00 +0500

    zadanie 1.6 19.11.2024

--- modul_1.6_peremenniye.py
+++ modul_1.6_peremenniye.py
@@ -3,4 +3,5 @@ kol_time = 1.5
 kurs_name = 'Python'
 time_na_1_zadanie = (kol_time/kol_dz)
 print(time_na_1_zadanie)
+
 print( kurs_name, ',', ' всего задач: ', kol_dz, ',', ' затрачено часов: ', kol_time, ',', ' среднее время выполнения задания ',  time_na_1_zadanie)
